517e16e8d5b887ff4cdf88995c6e0131d25a5dff
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2022-04-17 07:45:11 +0500

    :white_check_mark: fixed styling for Flatlist items

--- coloursBoxApp/components/ColorBox.js
+++ coloursBoxApp/components/ColorBox.js
@@ -1,44 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
-
-const COLORS = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb4b16' },
-  { colorName: 'Red', hexCode: '#dc322f' },
-  { colorName: 'Magenta', hexCode: '#d33682' },
-  { colorName: 'Violet', hexCode: '#6c71c4' },
-  { colorName: 'Blue', hexCode: '#268bd2' },
-  { colorName: 'Cyan', hexCode: '#2aa198' },
-  { colorName: 'Green', hexCode: '#859900' },
-];
+import { View, Text, StyleSheet } from 'react-native';
 
 const ColorBox = ({ colorName, hexCode }) => {
-  //   const boxColorStyle = {
-  //     backgroundColor: hexCode,
-  //   };
+  const boxColorStyle = {
+    backgroundColor: hexCode,
+  };
+  const textColor = {
+    color:
+      parseInt(hexCode.replace('#', ''), 16) > 0xffffff / 1.1
+        ? 'black'
+        : 'white',
+  };
   return (
-    <FlatList
-      data={COLORS}
-      renderItem={({ item }) => (
-        <View style={([styles.box], { backgroundColor: item.hexCode })}>
-          <Text style={styles.text}>{item.colorName}</Text>
-        </View>
-      )}
-      keyExtractor={(item) => item.hexCode}
-    />
-    // <View style={[styles.box, boxColorStyle]}>
-    //   <Text style={styles.text}>
-    //     {colorName} : {hexCode}
-    //   </Text>
-    // </View>
+    <View style={[styles.box, boxColorStyle]}>
+      <Text style={[styles.text, textColor]}>
+        {colorName} : {hexCode}
+      </Text>
+    </View>
   );
 };
 
